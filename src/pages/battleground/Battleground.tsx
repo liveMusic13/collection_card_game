a2c8f538bce6331b0eee7f25e3c95b59890d5f11
Dmitriy Kovalenko <115107980+liveMusic13@users.noreply.github.com>
2025-07-25 18:27:46 +0300

    add nav

--- src/pages/battleground/Battleground.tsx
+++ src/pages/battleground/Battleground.tsx
@@ -1,14 +1,19 @@
 import { type FC } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import GoldOnTheBattleground from '@/widgets/gold-on-the-battleground/GoldOnTheBattleground';
 import SideOfTheBattleground from '@/widgets/side-of-the-battleground/SideOfTheBattleground';
 
 const Battleground: FC = () => {
+	const nav = useNavigate();
+
 	return (
 		<div className='flex h-full'>
 			<div className='flex flex-col flex-1 bg-amber-300 '>
 				<SideOfTheBattleground isUser={false} />
 				<SideOfTheBattleground isUser={true} />
+				<Link to={'/'}>домашняя</Link>
+				<Link to={'/auth'}>авторизация</Link>
 			</div>
 			<GoldOnTheBattleground />
 		</div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import { type FC } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import AddDataInFirebase from '@/ADD_TEST_DATA/AddDataInFirebase';
 import { useAuth } from '@/features/auth/hooks/useAuth';
@@ -8,10 +9,14 @@ const Home: FC = () => {
 	useCheckAuth();
 	const { user } = useAuth();
 
+	const nav = useNavigate();
+
 	return (
 		<div>
 			Home
 			<AddDataInFirebase userId={user?.uid} />
+			<Link to={'/battleground'}>поле боя</Link>
+			<Link to={'/auth'}>авторизация</Link>
 		</div>
 	);
 };
